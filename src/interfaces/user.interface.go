e8315adec91508fab5b36d67ebf8db971a298c93
Akbar Mirza <143935492+akbar-mirza@users.noreply.github.com>
2026-04-15 12:08:46 +0530

    Fix userID mapping by adding BSON tags to user model

--- src/interfaces/user.interface.go
+++ src/interfaces/user.interface.go
@@ -1,12 +1,12 @@
 package interfaces
 
 type User struct {
-	Username     string `json:"username"`
-	Password     string `json:"password"`
-	SessionToken string `json:"sessionToken"`
-	CSRFToken    string `json:"csrfToken"`
-	UserID       string `json:"userID"`
-	LastLogin    int64  `json:"lastLogin"`
+	Username     string `json:"username" bson:"username"`
+	Password     string `json:"password" bson:"password"`
+	SessionToken string `json:"sessionToken" bson:"sessionToken"`
+	CSRFToken    string `json:"csrfToken" bson:"csrfToken"`
+	UserID       string `json:"userID" bson:"userID"`
+	LastLogin    int64  `json:"lastLogin" bson:"lastLogin"`
 }
 
 type CreateUser struct {
